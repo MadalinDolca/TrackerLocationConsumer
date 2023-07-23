b06a257f2e1e9f55c9aa956cd63c224512e5da31
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2023-07-24 00:20:07 +0300

    Client identification fix

--- app/src/main/java/com/madalin/trackerlocationconsumer/hivemq/ClientCredentials.kt
+++ app/src/main/java/com/madalin/trackerlocationconsumer/hivemq/ClientCredentials.kt
@@ -1,7 +1,7 @@
 package com.madalin.trackerlocationconsumer.hivemq
 
 object ClientCredentials {
-    const val clientId = "TrackerLocationSender"
+    const val clientId = "TrackerLocationConsumer"
     const val username = "location_consumer"
     const val password = "Consumer123"
 }
